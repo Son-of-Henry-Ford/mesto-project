0c196816a6b4f753c34d80443788e98a2919fdd6
Son-of-Henry-Ford <ford.rus2020@yandex.ru>
2025-05-15 03:30:07 +0300

    feat: добавить отображение карточек

--- scripts/index.js
+++ scripts/index.js
@@ -1,4 +1,19 @@
-// @todo: Темплейт карточки
+const cardTemplate = document.querySelector('#card-template').content;
+const cardsList = document.querySelector('.places__list');
+
+// наполняем содержимым
+initialCards.forEach(card => {
+    // клонируем содержимое тега template
+    const cardElement = cardTemplate.querySelector('.places__item').cloneNode(true);
+    //Наполнение контейнера
+    const cardImage = cardElement.querySelector('.card__image');
+    cardImage.src = card.link;
+    cardImage.alt = card.name;
+    cardElement.querySelector('.card__title').textContent = card.name;
+    // отображаем на странице
+    cardsList.append(cardElement);
+})
+
 
 // @todo: DOM узлы
 

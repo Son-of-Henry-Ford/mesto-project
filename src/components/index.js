c34a02587b15ee2660804ef6344b22dc305fe043
Son-of-Henry-Ford <ford.rus2020@yandex.ru>
2025-05-25 23:22:34 +0300

    feat:Загрузка информации о пользователе и загрузка карточек с сервера

--- src/components/index.js
+++ src/components/index.js
@@ -1,4 +1,3 @@
-import {initialCards} from "./cards.js";
 import {openModal, closeModal} from "./modal.js";
 import {createCard} from "./card.js";
 import {enableValidation} from "./validate.js";
@@ -15,6 +14,7 @@ const cardPopup = document.querySelector('.popup_type_new-card');
 const profile = document.querySelector('.profile');
 const userName = profile.querySelector('.profile__title');
 const userJob = profile.querySelector('.profile__description');
+const userAvatar = profile.querySelector('.profile__image');
 // Кнопки открытия/закрытия формы профиля
 const buttonOpenProfile = profile.querySelector('.profile__edit-button');
 const buttonCloseProfile = profilePopup.querySelector('.popup__close');
@@ -30,15 +30,19 @@ const cardUrlInput = cardPopup.querySelector('.popup__input_type_url');
 const cardFormElement = cardPopup.querySelector('.popup__form');
 
 
-// Функция отображения начальных карточек
-function displayCards() {
-// наполняем контейнер карточками.
-    initialCards.forEach(card => {
-        // отображаем на странице
-        cardsList.append(createCard(card.name, card.link));
-    })
+const validationSettings = {
+    formSelector: '.popup__form',
+    inputSelector: '.popup__input',
+    submitButtonSelector: '.popup__button',
+    inactiveButtonClass: 'popup__button_disabled',
+    inputErrorClass: 'popup__input_type_error',
+    errorClass: 'popup__error_visible'
 }
 
+const userConfig = {
+    groupId: 'apf-cohort-202',
+    accessToken: 'b7eca70d-a576-4b8a-81ae-597a37d75792'
+}
 
 // Обработчик отправки формы профиля
 function handleProfileFormSubmit(evt) {
@@ -46,7 +50,6 @@ function handleProfileFormSubmit(evt) {
     userName.textContent = nameInput.value;
     userJob.textContent = jobInput.value;
     closeModal(profilePopup);
-    displayCards();
 }
 
 
@@ -58,6 +61,64 @@ function handleCardFormSubmit(evt) {
     closeModal(cardPopup);
 }
 
+function addPopupAnimations() {
+    popups.forEach(popup => popup.classList.add("popup_is-animated"));
+}
+
+
+// Общая функция запроса
+function fetchData(url, config) {
+    return fetch(url, {
+        headers: {
+            authorization: config.accessToken
+        }
+    }).then(function (res) {
+        if (!res.ok) {
+            throw new Error("Ошибка: " + res.status);
+        }
+        return res.json();
+    });
+}
+
+
+// Получение пользователя
+function getUser(config) {
+    return fetchData("https://nomoreparties.co/v1/" + config.groupId + "/users/me", config);
+}
+
+
+// Получение карточек
+function getCards(config) {
+    return fetchData("https://nomoreparties.co/v1/" + config.groupId + "/cards", config);
+}
+
+
+// Установка данных пользователя
+function setUserInfo(config) {
+    getUser(config)
+        .then(function (user) {
+            userName.textContent = user.name;
+            userJob.textContent = user.about;
+            userAvatar.src = user.avatar;
+        })
+        .catch(function (error) {
+            console.error("Не удалось установить пользователя:", error);
+        });
+}
+
+
+// Работа с карточками
+function renderInitialCards(config) {
+    getCards(config)
+        .then(cards =>
+            cards.forEach(card => {
+                cardsList.append(createCard(card.name, card.link));
+            })
+        )
+        .catch(error =>
+            console.error("Ошибка при получении карточек:", error)
+        );
+}
 
 // Открытие формы редактирования профиля
 buttonOpenProfile.addEventListener('click', (e) => {
@@ -92,19 +153,12 @@ profileFormElement.addEventListener('submit', handleProfileFormSubmit);
 cardFormElement.addEventListener('submit', handleCardFormSubmit);
 
 
-displayCards();
-// Добавление анимаций для всех карточек
-popups.forEach(popup => {
-    popup.classList.add('popup_is-animated');
-})
+function initApp() {
+    setUserInfo(userConfig);
+    renderInitialCards(userConfig);
 
-const validationSettings = {
-    formSelector: '.popup__form',
-    inputSelector: '.popup__input',
-    submitButtonSelector: '.popup__button',
-    inactiveButtonClass: 'popup__button_disabled',
-    inputErrorClass: 'popup__input_type_error',
-    errorClass: 'popup__error_visible'
+    addPopupAnimations();
+    enableValidation(validationSettings);
 }
 
-enableValidation(validationSettings);
+initApp();

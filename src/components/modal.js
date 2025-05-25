f2f9169f165476ff43d11fe25fd586e5523a133a
Son-of-Henry-Ford <ford.rus2020@yandex.ru>
2025-05-25 04:23:25 +0300

    refactor: правки файлов связанных с карточками

--- src/components/modal.js
+++ src/components/modal.js
@@ -1,3 +1,6 @@
+let currentPopup = null;
+
+
 function closeByOverlay(e) {
     if (e.target === e.currentTarget) {
         closeModal(e.currentTarget);
@@ -6,15 +9,15 @@ function closeByOverlay(e) {
 
 
 function closeByEsc(e){
-    if(e.key === 'Escape') {
-        const openPopup = document.querySelector('.popup.popup_is-opened');
-        closeModal(openPopup);
+    if(e.key === 'Escape' && currentPopup !== null) {
+        closeModal(currentPopup);
     }
 }
 
 
 // Открытие модального окна
 export function openModal(popup) {
+    currentPopup = popup;
     popup.addEventListener('click', closeByOverlay);
     document.addEventListener('keydown', closeByEsc);
     popup.classList.add('popup_is-opened');
